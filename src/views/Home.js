dd9d78989ad150af0efb414ba08b0aac2ba84183
Manuel Venegas <mengasis@outlook.com>
2018-11-21 22:55:23 -0300

    Cleaning...

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import logo from '../logo.svg'
 
 const Home = () => {
 	return (
 		<div>
 			<span>HOME</span>
-			<img src={logo} />
 		</div>
 	)
 }
