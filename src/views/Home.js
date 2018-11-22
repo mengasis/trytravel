7d4eea11900adaca0575eec8e2c59aa4a0bdfc84
Manuel Venegas <mengasis@outlook.com>
2018-11-21 22:50:49 -0300

    Create Home layout

---
+++ src/views/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import logo from '../logo.svg'
+
+const Home = () => {
+	return (
+		<div>
+			<span>HOME</span>
+			<img src={logo} />
+		</div>
+	)
+}
+
+Home.propTypes = {}
+
+export default Home
