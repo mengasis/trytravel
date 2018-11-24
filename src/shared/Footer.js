d951f51ddb72ee554badb6e2f30eb7ff101b5c0f
Manuel Venegas <mengasis@outlook.com>
2018-11-23 23:48:57 -0300

    Renaming /common to /shared

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 const Footer = styled.div`
 	background-color: #000;
-	height: 100px;
 `
 
 export default Footer
--- src/shared/Header.js
+++ src/shared/Header.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 const Header = styled.div`
 	background-color: blue;
-	height: 150px;
 `
 
 export default Header
