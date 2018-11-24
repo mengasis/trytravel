216a16c37eb1f8d15329c8a8c1d2edaef171e134
Manuel Venegas <mengasis@outlook.com>
2018-11-24 02:17:11 -0300

    Separated in two rows the header

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
 const Header = styled.div`
-	background-color: blue;
+	background-color: purple;
+	display: grid;
+	grid-template-rows: 3fr 1fr;
+	grid-template-areas: 'header' 'nav';
 `
 
 export default Header
