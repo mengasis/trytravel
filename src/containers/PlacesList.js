67212940f54c9e6daaf01f6432419df09362de77
Manuel Venegas <mengasis@outlook.com>
2018-11-24 02:18:08 -0300

    Created PlacesList component

---
+++ src/containers/PlacesList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const PlacesList = () => {
+	return (
+		<div>
+			<span>List</span>
+		</div>
+	)
+}
+
+PlacesList.propTypes = {}
+
+export default PlacesList
