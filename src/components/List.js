5b821c398de1a2ea71fbce60701ba8108da27d01
Manuel Venegas <mengasis@outlook.com>
2018-11-25 03:07:22 -0300

    Grid styles for List component

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,11 @@
 import styled from 'styled-components'
 
 export default styled.section`
+	display: grid;
+	grid-auto-flow: dense;
+	grid-template-columns: repeat(5, 300px);
+	grid-template-rows: 450px 450px;
+	grid-auto-columns: 300px;
+	justify-items: start;
+	justialign-items: center;
 `
