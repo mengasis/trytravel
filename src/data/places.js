39fc49d5e3cfe1cb52f9334b608315cf550add05
Manuel Venegas <mengasis@outlook.com>
2018-11-25 03:09:25 -0300

    Added the images of the places

---
+++ src/data/places.js
@@ -0,0 +1,52 @@
+export default [
+	{
+		title: 'San Francisco',
+		url:
+			'https://images.unsplash.com/photo-1527905876394-1e5115891cba?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=03e900ad7822e824a78afd2c706ff241&auto=format&fit=crop&w=334&q=80'
+	},
+	{
+		title: 'New York',
+		url:
+			'https://images.unsplash.com/photo-1534256009774-91785b13a331?ixlib=rb-0.3.5&s=5e237cc060a187f7c2799ba1d6aa91b3&auto=format&fit=crop&w=634&q=80'
+	},
+	{
+		title: 'Miami',
+		url:
+			'https://images.unsplash.com/photo-1535498730771-e735b998cd64?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=623ed05afcc5a3fa1e444ffca95854dc&auto=format&fit=crop&w=334&q=80'
+	},
+	{
+		title: 'Moscow',
+		url:
+			'https://images.unsplash.com/photo-1520106212299-d99c443e4568?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4bafec2f2ac48819c9f7b02ed3950845&auto=format&fit=crop&w=334&q=80'
+	},
+	{
+		title: 'Paris',
+		url:
+			'https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f697e6b62b4b13af42ac059c235348f4&auto=format&fit=crop&w=334&q=80'
+	},
+	{
+		title: 'Rome',
+		url:
+			'https://images.unsplash.com/photo-1517655625604-e720102766b9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3d9ef9af2ae9a17c9a4b3edcafc3270f&auto=format&fit=crop&w=334&q=80'
+	},
+	{
+		title: 'New Zeland',
+		url:
+			'https://images.unsplash.com/photo-1526267888532-69091f908bf0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0ed9933e7a7337c6ab03d7f8fa1a1776&auto=format&fit=crop&w=334&q=80'
+	},
+	{
+		title: 'Japan',
+		url:
+			'https://images.unsplash.com/photo-1491884662610-dfcd28f30cfb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3298b33672eaddca009e5814fbfff498&auto=format&fit=crop&w=334&q=80'
+	},
+	{
+		title: 'London',
+		url:
+			'https://images.unsplash.com/photo-1526129318478-62ed807ebdf9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3f75b917098695dad49881631720a8f0&auto=format&fit=crop&w=334&q=80'
+	},
+	{
+		title: 'Istambul',
+		url:
+			'https://images.unsplash.com/photo-1527838832700-5059252407fa?ixlib=rb-0.3.5&s=eefb5a2040c092959cda152db91f8c2e&auto=format&fit=crop&w=343&q=80'
+	}
+]
