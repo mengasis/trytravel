423aa0342d33e40397067fefd14cd001308db82b
Manuel Venegas <mengasis@outlook.com>
2018-11-26 14:51:23 -0300

    New Box component with lazy load

---
+++ src/components/Box.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import LazyLoad from 'react-lazyload'
+
+const Container = styled.div`
+	border-radius: 5px;
+	padding: 10px;
+	background: #f2f2f2;
+	grid-row-end: span ${({ level }) => level || 1};
+`
+
+const Image = styled.img`
+	width: 100%;
+`
+const Box = ({ level, image }) => (
+	<LazyLoad height={100}>
+		<Container level={level}>
+			<Image src={image} />
+		</Container>
+	</LazyLoad>
+)
+
+Box.defaultProps = {
+	level: 1
+}
+
+Box.propTypes = {
+	level: PropTypes.number,
+	image: PropTypes.string.isRequired
+}
+
+export default Box
