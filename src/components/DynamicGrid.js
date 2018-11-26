67c74c49c7003777531384dc299fc5b0d08d5eeb
Manuel Venegas <mengasis@outlook.com>
2018-11-26 14:51:34 -0300

    Created DynamicGrid component

---
+++ src/components/DynamicGrid.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export default styled.section`
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+	grid-auto-rows: 40px;
+
+	justify-items: center;
+	gap: 10px;
+`
